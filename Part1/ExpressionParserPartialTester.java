dcf0e954b1b121735968fe3782ff579652d6afb5
Varnika Sinha <varnsinha@gmail.com>
2017-12-04 16:12:24 -0500

    Added more tests

--- Part1/ExpressionParserPartialTester.java
+++ Part1/ExpressionParserPartialTester.java
@@ -1,4 +1,5 @@
 package Part1;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,7 +36,7 @@ public class ExpressionParserPartialTester {
 	public void testExpression1 () throws ExpressionParseException {
 		final String expressionStr = "a+b";
 		final String parseTreeStr = "+\n\ta\n\tb\n";
-		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0));
+		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0).replace('*', '·'));
 	}
 
 	@Test
@@ -45,7 +46,7 @@ public class ExpressionParserPartialTester {
 	public void testExpression2 () throws ExpressionParseException {
 		final String expressionStr = "13*x";
 		final String parseTreeStr = "·\n\t13\n\tx\n";
-		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0));
+		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0).replace('*', '·'));
 	}
 
 	@Test
@@ -55,7 +56,7 @@ public class ExpressionParserPartialTester {
 	public void testExpression3 () throws ExpressionParseException {
 		final String expressionStr = "4*(z+5*x)";
 		final String parseTreeStr = "·\n\t4\n\t()\n\t\t+\n\t\t\tz\n\t\t\t·\n\t\t\t\t5\n\t\t\t\tx\n";
-		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0));
+		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0).replace('*', '·'));
 	}
 
 	@Test
@@ -65,6 +66,43 @@ public class ExpressionParserPartialTester {
 	public void testExpressionAndFlatten1 () throws ExpressionParseException {
 		final String expressionStr = "1+2+3";
 		final String parseTreeStr = "+\n\t1\n\t2\n\t3\n";
-		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0));
+		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0).replace('*', '·'));
+	}
+
+	@Test
+	/**
+	 * Verifies that a specific expression is parsed into the correct parse tree.
+	 */
+	public void testExpressionAndFlatten2 () throws ExpressionParseException {
+		final String expressionStr = "(x+(x)+(x+x)+x)";
+		final String parseTreeStr = "()\n\t+\n\t\tx\n\t\t()\n\t\t\tx\n\t\t()\n\t\t\t+\n\t\t\t\tx\n\t\t\t\tx\n\t\tx\n";
+		assertEquals(parseTreeStr, _parser.parse(expressionStr, false).convertToString(0).replace('*', '·'));
+	}
+
+	@Test(expected = ExpressionParseException.class) 
+	/**
+	 * Verifies that a specific expression is parsed into the correct parse tree.
+	 */
+	public void testException1 () throws ExpressionParseException {
+		final String expressionStr = "1+2+";
+		_parser.parse(expressionStr, false);
+	}
+
+	@Test(expected = ExpressionParseException.class) 
+	/**
+	 * Verifies that a specific expression is parsed into the correct parse tree.
+	 */
+	public void testException2 () throws ExpressionParseException {
+		final String expressionStr = "((()))";
+		_parser.parse(expressionStr, false);
+	}
+
+	@Test(expected = ExpressionParseException.class) 
+	/**
+	 * Verifies that a specific expression is parsed into the correct parse tree.
+	 */
+	public void testException3 () throws ExpressionParseException {
+		final String expressionStr = "()()";
+		_parser.parse(expressionStr, false);
 	}
 }
