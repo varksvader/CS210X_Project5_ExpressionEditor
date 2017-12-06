cc6cbf496a7ce0bd38457919e7e67d504d4827d4
Varnika Sinha <varnsinha@gmail.com>
2017-12-06 16:09:31 -0500

    made deepCopy() work need tests for it

--- Part1/SimpleCompoundExpression.java
+++ Part1/SimpleCompoundExpression.java
@@ -1,18 +1,7 @@
 package Part1;
 
-import java.util.LinkedList;
-
 public abstract class SimpleCompoundExpression extends AbstractCompoundExpression {
 
-    // Need to fix bc cannot instantiate abstract class
     @Override
-    public Expression deepCopy() {
-        final SimpleCompoundExpression copy = this; // need to find better solution
-
-        for (Expression child : children) {
-            copy.children.add(child.deepCopy());
-        }
-        return copy;
-    }
-
+    public abstract Expression deepCopy();
 }
