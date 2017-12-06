406901629a57373bffd0226b70871f99eeecfdb1
Varnika Sinha <varnsinha@gmail.com>
2017-12-06 18:31:23 -0500

    Added style edits

--- Part1/LiteralExpression.java
+++ Part1/LiteralExpression.java
@@ -1,37 +1,69 @@
 package Part1;
 
+/**
+ * CS 210X 2017 B-term (Sinha, Backe) 
+ * Expressions that are literals (also known as numbers [0-9]+ and variables [a-z])
+ */
 public class LiteralExpression implements Expression {
 
-    private String data;
-    private CompoundExpression parent;
+	private final String _value;
+	private CompoundExpression _parent;
 
-    public LiteralExpression(String data) {
-        this.data = data;
-    }
+	//Constructor
+	public LiteralExpression(String value) {
+		_value = value;
+	}
 
-    @Override
-    public CompoundExpression getParent() {
-        return parent;
-    }
+	/**
+	 * Returns the expression's parent.
+	 * @return the expression's parent
+	 */
+	@Override
+	public CompoundExpression getParent() {
+		return _parent;
+	}
 
-    @Override
-    public void setParent(CompoundExpression parent) {
-        this.parent = parent;
-    }
+	/**
+	 * Sets the parent be the specified expression.
+	 * @param parent the CompoundExpression that should be the parent of the target object
+	 */
+	@Override
+	public void setParent(CompoundExpression parent) {
+		_parent = parent;
+	}
 
-    @Override
-    public Expression deepCopy() {
-        return null;
-    }
+	/**
+	 * Creates and returns a deep copy of the expression.
+	 * The entire tree rooted at the target node is copied, i.e.,
+	 * the copied Expression is as deep as possible.
+	 * @return the deep copy
+	 */
+	@Override
+	public Expression deepCopy() {
+		return new LiteralExpression(_value);
+	}
 
-    @Override
-    public void flatten() { }
+	/**
+	 * Recursively flattens the expression as much as possible
+	 * throughout the entire tree. Specifically, in every multiplicative
+	 * or additive expression x whose first or last
+	 * child c is of the same type as x, the children of c will be added to x, and
+	 * c itself will be removed. This method modifies the expression itself.
+	 */
+	@Override
+	public void flatten() { /** Literal expressions have no children to flatten **/ }
 
-    @Override
-    public String convertToString(int indentLevel) {
-        StringBuffer buf = new StringBuffer("");
-        Expression.indent(buf, indentLevel);
-        buf.append(data + "\n");
-        return buf.toString();
-    }
+	/**
+	 * Creates a String representation by recursively printing out (using indentation) the
+	 * tree represented by this expression, starting at the specified indentation level.
+	 * @param indentLevel the indentation level (number of tabs from the left margin) at which to start
+	 * @return a String representation of the expression tree.
+	 */	
+	@Override
+	public String convertToString(int indentLevel) {
+		final StringBuffer buf = new StringBuffer("");
+		Expression.indent(buf, indentLevel);
+		buf.append(_value + "\n");
+		return buf.toString();
+	}
 }
