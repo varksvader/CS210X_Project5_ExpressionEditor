8e637864cf8cd08e7fd43708d9b036f8ad4b5726
Varnika Sinha <varnsinha@gmail.com>
2017-12-06 16:10:25 -0500

    made deepCopy() work need tests

--- Part1/MultiplicativeExpression.java
+++ Part1/MultiplicativeExpression.java
@@ -5,8 +5,19 @@ import java.util.LinkedList;
 public class MultiplicativeExpression extends SimpleCompoundExpression {
 
     public MultiplicativeExpression() {
-        data = "*";
+        operation = "*";
         children = new LinkedList<>();
     }
+
+	@Override
+	public Expression deepCopy() {
+		final Expression copy = new MultiplicativeExpression();
+		for (Expression e : this.children) {
+			((AbstractCompoundExpression) copy).addSubexpression(e.deepCopy());
+		}
+		return copy;
+	}
+    
+    
 
 }
