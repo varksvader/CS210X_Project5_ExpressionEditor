c2685743004208b6cd628a1bc4a3c5254853183f
Varnika Sinha <varnsinha@gmail.com>
2017-12-06 16:12:08 -0500

    made deepCopy work need more tests

--- Part1/AdditiveExpression.java
+++ Part1/AdditiveExpression.java
@@ -5,8 +5,17 @@ import java.util.LinkedList;
 public class AdditiveExpression extends SimpleCompoundExpression {
 
     public AdditiveExpression() {
-        this.data = "+";
+        operation = "+";
         children = new LinkedList<>();
     }
+
+	@Override
+	public Expression deepCopy() {
+		final Expression copy = new AdditiveExpression();
+		for (Expression e : this.children) {
+			((AbstractCompoundExpression) copy).addSubexpression(e.deepCopy());
+		}
+		return copy;
+	}
 
 }
